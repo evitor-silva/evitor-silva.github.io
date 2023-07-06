b627a9097f824fa4fe83908fc14f2092023c9a08
evitor-silva <zakrom656565@gmail.com>
2023-07-06 13:47:55 -0300

    up new

--- assets/ScriptMain.js
+++ assets/ScriptMain.js
@@ -1,6 +1,14 @@
 
 window.addEventListener('load', () => {
-    setTimeout(function() {
       document.querySelector('.loading-wrapper').style.display = 'none';
-  }, 500);
 })
+
+document.querySelector('.lista > li').addEventListener("click", ()=>{
+      document.querySelector('.menu-mobile').classList.toggle("show")
+})
+document.querySelector('.menu-close').addEventListener("click", ()=>{
+      document.querySelector('.menu-mobile').classList.toggle("show")
+})
+
+//console.log(document.querySelector("#github").offsetTop);
+//scroll({top: 200, behavior: "smooth"})
